b2ba1a3bb1859842b815ba0cf575875ba580e507
leilongxiao <2549314308@qq.com>
2020-10-18 23:56:19 +0800

    [增加] 乐优商城，文件（图片）上传功能。

---
+++ leyou-upload/src/main/java/com/leyou/upload/service/UploadService.java
@@ -0,0 +1,48 @@
+package com.leyou.upload.service;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class UploadService {
+    private static final List<String> CONTENT_TYPES = Arrays.asList("image/jpg", "image/jpeg", "image/png");
+    public static final Logger LOGGER = LoggerFactory.getLogger(UploadService.class);
+
+    public String uploadImage(MultipartFile file){
+        String originalFilename = file.getOriginalFilename();
+        //1.文件类型
+        String contentType = file.getContentType();
+        if (!CONTENT_TYPES.contains(contentType)) {
+            LOGGER.info("文件上传失败：{},文件类型不合法", originalFilename);
+            return null;
+        }
+        try {
+            //2.校验文件内容
+            BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
+            if (bufferedImage == null) {
+                LOGGER.info("文件上传失败：{},文件内容不合法", originalFilename);
+                return null;
+            }
+
+            //3.保存到服务器
+            file.transferTo(new File("D:\\IdeaProjects\\code\\leyou\\images\\"+originalFilename));
+            //4.返回url路径
+
+            return "http://image.leyou.com/"+originalFilename;
+        } catch (IOException e) {
+            LOGGER.info("文件上传失败：{},服务器异常！{}", originalFilename,e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
